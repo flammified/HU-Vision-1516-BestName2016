5378d93c890fb758e95b474cc670a0f01a8e99ef
Alexander Freeman <rubberduck313@gmail.com>
2016-05-06 11:00:07 +0200

    Created studentkernel class

---
+++ source/ExternalDLL/ExternalDLL/StudentKernel.cpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <memory>
+#include "StudentKernel.h"
+IntensityImageStudent StudentKernel::apply_on_image(IntensityImageStudent img) {
+	for (int y = 0; y < img.getHeight(); y++) {
+		for (int x = 0; x < img.getWidth(); x++) {
+
+			int temp = 0;
+
+			for (int y_k = 0; y_k < this->h; y_k++) {
+				for (int x_k = 0; x_k < this->w; x_k++) {
+				
+					int dx = x_k - 1;
+					int dy = y_k - 1;
+
+					Intensity pix_val = img.getPixel(x_k + dx, y_k + dy);
+					
+					temp += (pix_val * values[dy][dx]);
+				}
+			}
+
+			img.setPixel(x, y, temp * factor);
+		}
+	}
+	return img;
+}
---
+++ source/ExternalDLL/ExternalDLL/StudentKernel.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+#include "IntensityImageStudent.h"
+
+class StudentKernel {
+public:
+	StudentKernel(double** values, int w, int h) : values(values), w(w), h(h), factor(1) {}
+	StudentKernel(double** values, int w, int h, double factor) : values(values), w(w), h(h), factor(factor) {}
+	IntensityImageStudent apply_on_image(IntensityImageStudent img);
+private:
+	double ** values;
+	int w, h;
+	double factor;
+};
