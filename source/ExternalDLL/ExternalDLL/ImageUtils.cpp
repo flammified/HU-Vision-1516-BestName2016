c3d12e5eebf60a1c622db91e564053f8be74d226
Alexander Freeman <rubberduck313@gmail.com>
2016-05-15 20:26:44 +0200

    Added zero occurrence method

--- source/ExternalDLL/ExternalDLL/ImageUtils.cpp
+++ source/ExternalDLL/ExternalDLL/ImageUtils.cpp
@@ -18,6 +18,47 @@ IntensityImageStudent ImageUtils::subimage(const IntensityImage * image, const P
 	return result;
 }
 
+IntensityImageStudent ImageUtils::zero_crossings(const IntensityImage * image)
+{
+	IntensityImageStudent result(*image);
+
+	int deltas[4][2] = {
+		{1, 0},
+		{0, 1},
+		{-1, 0},
+		{0, -1}
+	};
+
+	for (int x = 0; x < image->getWidth(); x++) {
+		for (int y = 0; y < image->getHeight(); y++) {
+			bool change_in_range = false;
+			bool lowest = true;
+			int current_pixel = image->getPixel(x, y);
+
+			for (int i = 0; i < 4; i++) {
+
+				int dx = deltas[i][0];
+				int dy = deltas[i][1];
+				if (x + dx < 0 || x + dx > image->getWidth()) continue;
+				if (y + dy < 0 || y + dy > image->getHeight()) continue;
+
+				int pixel_value = image->getPixel(x + dx, y + dy) - 127;
+
+				bool c = ((pixel_value - 127) ^ (current_pixel - 127)) < 0;
+
+				if (c) {
+					if (abs(pixel_value) > abs(current_pixel)) {
+						lowest = false;
+					}
+					change_in_range = true;
+				}
+			}
+			result.setPixel(x, y, change_in_range && lowest ? 255 : 0);
+		}
+	}
+	return result;
+}
+
 StudentHistogram ImageUtils::histogram_from_x_axis(const IntensityImage * image, int y)
 {
 	StudentHistogram histo(image->getWidth());
